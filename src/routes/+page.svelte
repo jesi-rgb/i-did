d537cf4095bdb7a9c22a1ba87a76c2d96e632316
Jesús Enrique Rascón <blograso@gmail.com>
2024-03-10 00:50:33 +0100

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { type DateValue, DateFormatter, getLocalTimeZone } from '@internationalized/date';
+
+	import { Textarea } from '$lib/components/ui/textarea';
+
+	const df = new DateFormatter('en-US', {
+		dateStyle: 'long'
+	});
+
+	let value: DateValue | undefined = undefined;
+</script>
+
+<div class="absolute bottom-10 left-0 w-1/2 translate-x-1/2">
+	<Textarea />
+</div>
