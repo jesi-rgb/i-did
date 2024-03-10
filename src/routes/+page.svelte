99527233b52684405f3ff6d425dadd58574b1680
Jesús Enrique Rascón <blograso@gmail.com>
2024-03-10 12:36:08 +0100

    idk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,91 @@
 <script lang="ts">
-	import { type DateValue, DateFormatter, getLocalTimeZone } from '@internationalized/date';
+	import Send from 'lucide-svelte/icons/send';
+	import winkNLP from 'wink-nlp';
+	import model from 'wink-eng-lite-web-model';
+
+	import { list } from '../stores.js';
 
 	import { Textarea } from '$lib/components/ui/textarea';
 
-	const df = new DateFormatter('en-US', {
-		dateStyle: 'long'
-	});
+	import type { HTMLTextareaAttributes } from 'svelte/elements';
+	import Badge from '@/components/ui/badge/badge.svelte';
+	import Input from '@/components/ui/input/input.svelte';
+	import ScrollArea from '@/components/ui/scroll-area/scroll-area.svelte';
+	import Button from '@/components/ui/button/button.svelte';
+	import CalendarNextButton from '@/components/ui/calendar/calendar-next-button.svelte';
 
-	let value: DateValue | undefined = undefined;
+	const nlp = winkNLP(model);
+	const its = nlp.its;
+	const as = nlp.as;
+
+	let value: string;
+	let textArea: HTMLTextareaAttributes;
+
+	const monthColor = {
+		aug: 'red'
+	};
+
+	const isDateTime = {
+		DATE: true,
+		TIME: true
+	};
+
+	let items: string[] = [];
+	function processItem() {
+		const doc = nlp.readDoc(value);
+
+		doc
+			// extract entities,
+			.entities()
+			// filter DATE & TIME,
+			.filter((e) => isDateTime[e.out(its.type)])
+			// and finally markup with bold tag with class as entity type.
+			.each((e) => {
+				e.markup(
+					`<span class:${monthColor[e.tokens()[0]]} class="bg-primary px-1.5 py-0.5 rounded-md text-primary-foreground" id='${e.out(its.type)}'>`,
+					`</span>`
+				);
+			});
+
+		$list.push(doc.out(its.markedUpText));
+		$list = $list;
+		value = ''.trim();
+	}
+	function sendItem(event: KeyboardEvent) {
+		if (value === '' || value === undefined) return;
+		if (event.key === 'Enter') {
+			processItem();
+		}
+	}
+
+	function sendItemButton(event: Event) {
+		if (value === '' || value === undefined) return;
+		processItem();
+	}
+
+	function resetStore() {
+		$list = [];
+	}
 </script>
 
-<div class="absolute bottom-10 left-0 w-1/2 translate-x-1/2">
-	<Textarea />
-</div>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+</head>
+
+<main class="mx-auto my-5 w-[90%] md:my-20 md:w-1/2">
+	<ul class="my-10 flex h-[600px] list-inside list-disc flex-col space-y-2 overflow-y-scroll">
+		{#each $list as item}
+			<li class="list-item">{@html item}</li>
+		{/each}
+	</ul>
+	<div class="sticky bottom-1 w-full md:bottom-10">
+		<div class="my-2 flex gap-3">
+			<Input bind:this={textArea} bind:value on:keydown={sendItem} />
+			<Button on:click={sendItemButton} variant="outline">
+				<Send />
+			</Button>
+		</div>
+
+		<Button variant="destructive" on:click={resetStore}>Reset</Button>
+	</div>
+</main>
---
+++ src/stores.js
@@ -0,0 +1,3 @@
+import { persisted } from 'svelte-persisted-store';
+
+export const list = persisted('list', []);
